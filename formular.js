ed3ff311cb8be6c7ffd437ebe0752e97de3c4fdf
Weyllan <valentin.bequart@isen-lille.fr>
2017-11-08 14:43:21 +0100

    add some details for adress

--- formular.js
+++ formular.js
@@ -1,3 +1,16 @@
 function secondStep(){
-  console.log("Bonjour!");
+  var name = document.getElementById("name").value;
+  var firstname = document.getElementById("firstname").value;
+  var number = document.getElementById("phone").value;
+  var long2 = document.getElementById("long2").value;
+  if (lat != "Latitude1" && long != "Longitude1" && lat2 != "Latitude2" && long2 != "Longitude2"){
+    var coord = [];
+    coord.push(lat);
+    coord.push(long);
+    coord.push(lat2);
+    coord.push(long2);
+    //socket.emit('coordinates', coord);
+  } else {
+    alert("Vous avez oublié des coordonnées!");
+  }
 }
