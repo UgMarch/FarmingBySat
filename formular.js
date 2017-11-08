5e2a6ef6092924aa380c724b17d2420f3a9d971e
Weyllan <valentin.bequart@isen-lille.fr>
2017-11-08 15:14:44 +0100

    add mail, and security for the formular

--- formular.js
+++ formular.js
@@ -2,8 +2,36 @@ function secondStep(){
   var name = document.getElementById("name").value;
   var firstname = document.getElementById("firstname").value;
   var number = document.getElementById("phone").value;
-  var long2 = document.getElementById("long2").value;
-  if (lat != "Latitude1" && long != "Longitude1" && lat2 != "Latitude2" && long2 != "Longitude2"){
+  var adress = document.getElementById("adress").value;
+  var postalCode = document.getElementById("postalCode").value;
+  var city = document.getElementById("city").value;
+  if(name.length >= 2 && name.length <= 20){
+    if(firstname.length >= 2 && firstname.length <= 20){
+      if(number.length == 10 && isNaN(number) == false){
+        if(adress.length > 3){
+          if(postalCode.length == 5 && isNaN(postalCode) == false){
+            if(city.length > 0){
+              console.log(name+firstname+number+adress+postalCode+city);
+            }else{
+              alert("Le champ de Ville n'est pas bien rempli!");
+            }
+          }else{
+            alert("Le code postal n'est pas bien rempli!");
+          }
+        }else{
+          alert("L'adresse n'est pas bien saisie!");
+        }
+      }else{
+        alert("Le numéro n'est pas valide!");
+      }
+    }else{
+      alert("Le prénom est invalide");
+    }
+  }else{
+    alert("Le nom est invalide");
+  }
+
+  /*if (lat != "Latitude1" && long != "Longitude1" && lat2 != "Latitude2" && long2 != "Longitude2"){
     var coord = [];
     coord.push(lat);
     coord.push(long);
@@ -12,5 +40,5 @@ function secondStep(){
     //socket.emit('coordinates', coord);
   } else {
     alert("Vous avez oublié des coordonnées!");
-  }
+  }*/
 }
