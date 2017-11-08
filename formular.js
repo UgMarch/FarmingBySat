be4b135b2fe9fc5a19c11330b7d946d39fe8476d
Weyllan <valentin.bequart@isen-lille.fr>
2017-11-08 15:27:33 +0100

    finalize verification

--- formular.js
+++ formular.js
@@ -5,13 +5,19 @@ function secondStep(){
   var adress = document.getElementById("adress").value;
   var postalCode = document.getElementById("postalCode").value;
   var city = document.getElementById("city").value;
+  var mail = document.getElementById("mail").value;
   if(name.length >= 2 && name.length <= 20){
     if(firstname.length >= 2 && firstname.length <= 20){
       if(number.length == 10 && isNaN(number) == false){
         if(adress.length > 3){
           if(postalCode.length == 5 && isNaN(postalCode) == false){
             if(city.length > 0){
-              console.log(name+firstname+number+adress+postalCode+city);
+              var regmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+              if(regmail.test(mail)){
+                console.log(name+firstname+number+adress+postalCode+city);
+              }else{
+                alert("Champ mail invalide!");
+              }
             }else{
               alert("Le champ de Ville n'est pas bien rempli!");
             }
