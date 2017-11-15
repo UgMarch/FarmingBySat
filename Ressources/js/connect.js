4b7d588ea7cb29cbd744dded3433565cac5557f9
Weyllan <valentin.bequart@isen-lille.fr>
2017-11-15 14:14:53 +0100

    re-etablish connection between client and servor

---
+++ Ressources/js/connect.js
@@ -0,0 +1,44 @@
+var socket = io.connect('http://localhost:8080');
+
+socket.on('message', function(message) {
+  alert('Le serveur a un message pour vous : '+message);
+});
+
+$('#poke').click(function () {
+  socket.emit('message', 'Salut Serveur, ça va?');
+});
+
+function extract() {
+  socket.emit('extractFile', "Copernicus sends us some datas");
+}
+
+function sendData() {
+
+  var lat = document.getElementById("lat").value;
+  var long = document.getElementById("long").value;
+  var lat2 = document.getElementById("lat2").value;
+  var long2 = document.getElementById("long2").value;
+  if (lat != "Latitude1" && long != "Longitude1" && lat2 != "Latitude2" && long2 != "Longitude2"){
+    var coord = [];
+    coord.push(lat);
+    coord.push(long);
+    coord.push(lat2);
+    coord.push(long2);
+    socket.emit('coordinates', coord);
+  } else {
+    alert("Vous avez oublié des coordonnées!");
+  }
+
+}
+
+function deleteData() {
+  socket.emit('deleteData', "Asked me to delete raws!");
+}
+
+function executeJ() {
+  socket.emit('execJava', 'Lets convert those images!');
+}
+
+function convertJP2() {
+  socket.emit('findImagesName', 'Me too!');
+}
